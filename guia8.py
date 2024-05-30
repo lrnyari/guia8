c80e090198b38eeb57076807345e04a9870f6735
Lisandro <lisandronyari@gmail.com>
2024-05-29 21:02:33 -0300

    maximo

--- guia8.py
+++ guia8.py
@@ -27,13 +27,36 @@ def contar_elementos_pila(p: Pila) -> int:
     
     return cantidad
 
+#ESTÀ MAL ROMPE CANTIDAD
+#def generar_nros_al_azar(cantidad: int, desde: int, hasta: int) -> Pila:
+#    p = Pila()
+#    while(cantidad > 0):
+#        p.put(random.randint(desde,hasta))
+#        cantidad -= 1
+#    
+#    return p
+
 def generar_nros_al_azar(cantidad: int, desde: int, hasta: int) -> Pila:
-    p = Pila()
-    while(cantidad > 0):
-        p.put(random.randint(desde,hasta))
-        cantidad -= 1
+    pila_res = Pila()
+
+    for _ in range(cantidad):
+        valor:int = random.randint(desde, hasta)
+        pila_res.put(valor)
     
-    return p
+    return pila_res
+
+def buscar_el_maximo(p: Pila) -> int:
+    paux = copiar_pila(p)
+    maximo:int = 0
+    aux:int = 0
+    while(not paux.empty()):
+        aux = paux.get()
+        if(aux > maximo):
+            maximo = aux
+    return maximo
+        
+
+
 
 mi_pila = Pila()
 mi_pila.put(2)
@@ -43,4 +66,6 @@ mi_pila.put(9)
 print(contar_elementos_pila(mi_pila))
 print(contar_elementos_pila(mi_pila))
 
-print(list(generar_nros_al_azar(4,0,9).queue))
+#print(list(generar_nros_al_azar(4,0,9).queue))
+print(generar_nros_al_azar(4,0,9).queue)
+print(buscar_el_maximo(mi_pila))
